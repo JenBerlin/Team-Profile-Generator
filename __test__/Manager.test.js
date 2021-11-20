6bf816673bc28f5797b4b2ab1aacae3a321c029c
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-11-20 19:12:31 +0100

    status for today

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -6,3 +6,11 @@ describe("Manager", () => {
     expect(employee).toBeTruthy();
   });
 });
+
+//  it("Creates an manager with name, id and email", () => {
+//    const employee = new Manager ("Bob", 42, "info@info.com", 1);
+//    expect(employee.getName()).toEqual("Bob");
+//    expect(employee.getId()).toEqual(42);
+//    expect(employee.getEmail()).toEqual("info@info.com");
+//              office number testen
+//  });
---
+++ index.js
@@ -0,0 +1,11 @@
+// requier
+
+const team = [];
+
+// Fragen einbauen
+
+const Bob = new Manager("Tim", 3, "tim@info.com", 4);
+
+team.push(Bob);
+
+// writeFile
